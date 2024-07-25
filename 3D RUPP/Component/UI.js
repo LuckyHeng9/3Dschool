1c200885a6f80c95b7e3e9d2b54794c5f5d1f54b
LuckyHeng9 <loemheng123kh@gmail.com>
2024-07-25 20:51:50 +0700

    Delete 3D RUPP directory

--- 3D RUPP/Component/UI.js
+++
@@ -1,5 +0,0 @@
-// const closeButton = document.querySelector('#closeButton');
-//         closeButton.addEventListener('click', function() {
-//             const card = document.querySelector('#card');
-//             card.style.display = 'none';
-//         });
